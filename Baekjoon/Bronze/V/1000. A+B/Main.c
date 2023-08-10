73085d7d7a5a180b8b3c812cf3a503635a7f07fc
pikaybh <pikabh@naver.com>
2023-08-11 06:28:56 +0900

    C, js 성공; C# 실패

---
+++ Baekjoon/Bronze/V/1000. A+B/Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @brief 
+ * FileName :  Main.c
+ * Developer : @pikaybh
+ */
+
+int main(void) {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A + B);
+
+    return 0;
+}
---
+++ Baekjoon/Bronze/V/1000. A+B/Main.cs
@@ -0,0 +1,16 @@
+/**
+using System;
+using static System.Console;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int[] n_array = Console.ReadLine().Split(' ');
+        }
+        Console.WriteLine(n_array.Sum());
+    }
+}
+*/
